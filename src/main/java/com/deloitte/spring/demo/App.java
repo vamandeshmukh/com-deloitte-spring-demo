4f3670c5b5048669ef013f3574242b5a03428fe8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 16:40:46 +0530

    spring-core

--- src/main/java/com/deloitte/spring/demo/App.java
+++ src/main/java/com/deloitte/spring/demo/App.java
@@ -15,7 +15,11 @@ public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println("Start");
+		
+		
+
+		System.out.println("End");
 
 	}
 }
