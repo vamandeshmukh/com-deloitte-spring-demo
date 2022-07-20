a00fea8207fdff92b38641de7093acc91019237e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-20 15:55:09 +0530

    spring-core

--- src/main/java/com/deloitte/spring/demo/config/SpringConfig.java
+++ src/main/java/com/deloitte/spring/demo/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.deloitte.spring.demo.config;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,8 @@ public class SpringConfig {
 //		return new Employee();
 //		return new Employee(101, "Sonu", 95000);
 		return new Employee(101, "Sonu", 95000, 
-	new ArrayList<>(), new Department(10, "HR", "Hyderabad"));
+new ArrayList<>(Arrays.asList("Banking", "Finance", "Stocks")),
+				new Department(10, "HR", "Hyderabad"));
 
 	}
 
